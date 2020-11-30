675001035bce7638989ec8660081da5adde158ed
unknown <j0s3.90jr@gmail.com>
2020-11-30 07:21:32 +0100

    Principal

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+const menuLateral = document.getElementById('menu-lateral');
+const menuLateralLista = document.getElementById('menu-lateral_lista');
+const iconoMenuLateral = document.getElementById('icono-menu_lateral');
+
+const mostrarOcultarMenu = () => {
+    let menuVisible = document.getElementsByClassName('menu-lateral_visible');
+    if (menuVisible.length > 0){
+        menuLateral.classList.remove('menu-lateral_visible');
+    }
+    else {
+        menuLateral.classList.add('menu-lateral_visible');
+    }
+}
+
+iconoMenuLateral.addEventListener('click', mostrarOcultarMenu);
